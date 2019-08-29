35806da9097bdbb2e3ddd7475ad12c0657708b36
veldic <rsual@snu.ac.kr>
2019-08-29 16:59:38 +0900

    Add SNS Links

--- app/src/main/java/com/example/wafflestrecruting/SecondActivity.java
+++ app/src/main/java/com/example/wafflestrecruting/SecondActivity.java
@@ -2,7 +2,14 @@ package com.example.wafflestrecruting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void goToInstagram(View view) {
+        Uri webpage = Uri.parse("https://www.instagram.com/square_min_v/");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(webIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if(isIntentSafe) {
+            startActivity(webIntent);
+        }
+    }
+
+    public void goToFacebook(View view) {
+        Uri webpage = Uri.parse("https://www.facebook.com/profile.php?id=100006686296910");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(webIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if(isIntentSafe) {
+            startActivity(webIntent);
+        }
     }
 }
